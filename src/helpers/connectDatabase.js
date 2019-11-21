bebe9567c384dccf916e6430e8cfcbfeef383d43
Phan Phu <lacmatlaban@gmail.com>
2019-11-21 14:51:07 +0700

    update

--- src/helpers/connectDatabase.js
+++ src/helpers/connectDatabase.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 function getDatabaseUri() {
     if (process.env.NODE_ENV === 'production')
-        return 'mongodb+srv://phu:phu147@cluster0-haxly.mongodb.net/test?retryWrites=true&w=majority';
+        return 'mongodb+srv://test:phu123@phanphu-ktiiq.mongodb.net/test?retryWrites=true&w=majority';
     // return 'mongodb://localhost:27017/manageLibrary';
-    return 'mongodb+srv://phu:phu147@cluster0-haxly.mongodb.net/test?retryWrites=true&w=majority';
+    return 'mongodb+srv://test:phu123@phanphu-ktiiq.mongodb.net/test?retryWrites=true&w=majority';
 }
 mongoose.set('useFindAndModify', false);
 mongoose.connect(getDatabaseUri(), {
