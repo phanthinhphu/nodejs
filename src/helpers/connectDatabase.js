2b23edece01b6c475469b015b836b57fc5592605
Phan Phu <lacmatlaban@gmail.com>
2019-11-21 21:35:02 +0700

    make it better

--- src/helpers/connectDatabase.js
+++ src/helpers/connectDatabase.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 function getDatabaseUri() {
     if (process.env.NODE_ENV === 'production')
-        return 'mongodb+srv://test:phu123@phanphu-ktiiq.mongodb.net/test?retryWrites=true&w=majority';
-    // return 'mongodb://localhost:27017/manageLibrary';
-    return 'mongodb+srv://test:phu123@phanphu-ktiiq.mongodb.net/test?retryWrites=true&w=majority';
+        return 'mongodb://phanthinhphu:phanthinhphu147@ds219055.mlab.com:19055/library';
+    return 'mongodb://localhost:27017/manageLibrary';
+    //return 'mongodb+srv://test:phu123@phanphu-ktiiq.mongodb.net/test?retryWrites=true&w=majority';
 }
 mongoose.set('useFindAndModify', false);
 mongoose.connect(getDatabaseUri(), {
