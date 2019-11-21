8327092ba1758f1f4ef7d47a010c0e029b3ca8fb
Phan Phu <lacmatlaban@gmail.com>
2019-11-21 14:24:12 +0700

    make it better

--- src/helpers/connectDatabase.js
+++ src/helpers/connectDatabase.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 function getDatabaseUri() {
     if (process.env.NODE_ENV === 'production')
         return 'mongodb+srv://phu:phu147@cluster0-haxly.mongodb.net/test?retryWrites=true&w=majority';
-    return 'mongodb://localhost:27017/manageLibrary';
+    // return 'mongodb://localhost:27017/manageLibrary';
+    return 'mongodb+srv://phu:phu147@cluster0-haxly.mongodb.net/test?retryWrites=true&w=majority';
 }
 mongoose.set('useFindAndModify', false);
 mongoose.connect(getDatabaseUri(), {
