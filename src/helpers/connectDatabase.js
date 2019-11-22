1f439137562114e25bfc55ad3b27e52d5deec394
Phan Phu <lacmatlaban@gmail.com>
2019-11-22 18:39:20 +0700

    update

--- src/helpers/connectDatabase.js
+++ src/helpers/connectDatabase.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 function getDatabaseUri() {
-    // if (process.env.NODE_ENV === 'production')
+    // if (process.env.NODE_ENV === 'production1')
     //     return 'mongodb://phanthinhphu:phanthinhphu147@ds219055.mlab.com:19055/library';
     // return 'mongodb://localhost:27017/manageLibrary';
-    return 'mongodb://phuzz:phu123@ds161446.mlab.com:61446/phuz';
+   // return 'mongodb://phuzz:phu123@ds161446.mlab.com:61446/phuz';
 }
 mongoose.set('useFindAndModify', false);
 mongoose.connect(getDatabaseUri(), {
